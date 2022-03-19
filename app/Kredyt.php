e9f2369d08a7bdb6bf85f3ff52d6cb22190f0fd9
ThunderStorm24 <patryko5@onet.pl>
2022-03-19 12:55:16 +0100

    Wprowadzenie szablonu

--- app/Kredyt.php
+++ app/Kredyt.php
@@ -2,24 +2,36 @@
 <head>
 <meta charset="utf-8" />
 <title>Kredyt</title>
+
+ <link rel="stylesheet" type="text/css" href="<?php print(_APP_URL);?>/app/css/style.css" />
+ 
 </head>
-<body style="background-color: #99CCFF">
 
-<?php echo "<p style='text-align: right'> Jesteś zalogowany jako: <b>".$role."</b>"; ?><br>
-<a href="<?php print(_APP_ROOT); ?>/app/security/logout.php">Wyloguj</a></p>
-
-<div style="background-color: #FFFFFF">
-<h1>Kredyt - kalkulator</h1>
-<form action="<?php print(_APP_URL);?>/app/Srodek.php" method="post">
-<label>Kwota:</label>
-<input type="Number" id="kwota" name="kwo" value="<?php echo $k; ?>" min="1" placeholder="1-ile chcesz"></input><br>
-<label>Lata:</label>
-<input type="Number" id="rok" name="lat" value="<?php echo $l; ?>" min="1" max="30" placeholder="1-30"></input><br>
-<label>Procenty:</label>
-<input type="Number" id="procent" name="pro" value="<?php echo $p; ?>" min="0" max="100" placeholder="0-100"></input>%<br>
-<input type="submit" value="Oblicz"></input>
-</form>
-
+<body>
+  <div id="wrapper">
+    <div id="logo">
+    <img src="<?php print(_APP_URL);?>/app/images/logo.png" />
+    </div>
+    <div id="menu">
+        <ul>
+        <li><a href="#" class="active">Strona główna</a></li>
+        <li><a href="<?php print(_APP_ROOT); ?>/app/security/logout.php"><?php echo "Jesteś zalogowany jako: <b>".$role."</b>"; ?><br></a></li>
+        </ul>
+    </div>
+    <div id="header">
+    <img src="<?php print(_APP_URL);?>/app/images/bg_head.jpg" style="float: right" />
+    <div id="title_head">    
+    <h2>WKT - Wspaniałe Kredyty u Thundera</h2>
+    <h3>Kredyt - Kalkulator</h3>
+    <form action="<?php print(_APP_URL);?>/app/Srodek.php" method="post">
+        <label>Kwota:</label>
+        <input type="Number" id="kwota" name="kwo" value="<?php echo $k; ?>" min="1" placeholder="1-ile chcesz"></input><br>
+        <label>Lata:</label>
+        <input type="Number" id="rok" name="lat" value="<?php echo $l; ?>" min="1" max="30" placeholder="1-30"></input><br>
+        <label>Procenty:</label>
+        <input type="Number" id="procent" name="pro" value="<?php echo $p; ?>" min="0" max="100" placeholder="0-100"></input>%<br>
+        <input type="submit" value="Oblicz"></input>
+    </form>
 <?php
 //Jeśli są błędy, wyświetl je
 if(isset ($message)){
@@ -41,11 +53,10 @@ echo 'BRAK PODATKU';
 }
 ?>
 
-<br><br><a href="<?php print(_APP_ROOT); ?>/app/inna_chroniona.php">Kolejna chroniona strona</a>
-</div>
-
-<div style="background-color: #FFFFFF">
-<h2><p style="text-align:center">Opis</p></h2>
+    </div>    
+    </div>
+    <div id="solid">
+    <h2><p style="text-align:center;font-size:20px;"><b>Opis:</b></p></h2>
 <p style="margin-left:15px;">Uzytkownik ma mniejsze uprawnienia od admina. Admin moze wykonywac obliczenia zwiazane z kredytem na wieksza skale. Uzytkownik natomiast ma pewne ograniczenia. Jesli masz jakis problem pisz do supportu pod adresem kredyt24hnadobe@gmail.com lub dzwon do nas pod numer 505 213 777. Za wszelkie problemy i niedogodności przepraszamy. Ponizej dane ktore moze wprowadzac uzytkownik a admin:</p>
 <ol> <b> USER </b>
     <li>Limit kwoty: 1-10000</li>
@@ -58,5 +69,8 @@ echo 'BRAK PODATKU';
     <li>Limit Procent: 0-100%</li>
 </ol><br>
 </div>
+    <div id="footer">
+        <p>&copy Wszelkie prawa zastrzeżone. <br /> Projekt: <a href="http://www.templatki.net">Darmowe Szablony Stron</a></p>
+    </div>
 </body>
-</html> 
+</html>
